91773e141b143a81f7c26c6e6bd98b86254fe26c
Wayne Wang <iverson0217@gmail.com>
2020-04-01 21:46:02 +0800

    finishing sign in functionality

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './signin.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -22,9 +22,15 @@ class SignIn extends React.Component{
         });
     }
 
-    handleSubmit = (event)=>{
+    handleSubmit = async (event) =>{
         event.preventDefault();
-        this.setState({ email: '', password: '' });
+        const {email,password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '', password: '' });
+        }catch(error){
+
+        }
     }
 
     render(){
