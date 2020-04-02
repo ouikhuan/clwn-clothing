26c8013fbfa1e878074f5e6d177efc50b021684c
Wayne Wang <iverson0217@gmail.com>
2020-04-02 17:12:13 +0800

    Adding reselect, making items count and cartitem using memoir selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,16 +3,21 @@ import {connect} from 'react-redux';
 import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingBagIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
+
+const mapStateToProps = (state) => ({
+    itemCount:selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch)=> ({
     toggleCartHidden:()=>dispatch(toggleCartHidden())
 });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
