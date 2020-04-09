3af2f9e5c1af00fe58942c08a78e29b105f60b29
Wayne Wang <iverson0217@gmail.com>
2020-04-09 15:20:13 +0800

    Adding express sslify

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== "production") require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 if(process.env.NODE_ENV === "production"){
+
+    app.use(compression);
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
     app.use(express.static(path.join(__dirname,'client/build')));
     app.get('*',function(req,res){
         res.sendFile(path.join(__dirname,'client/build','index.html'));
